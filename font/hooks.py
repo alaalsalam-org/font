0d789c82a9f00b2b6d6a0b379dae6e9f225c1bf2
MohamedAbdulsalam96 <moha2016it@gmail.com>
2024-01-16 22:05:03 +0100

    init App

--- font/hooks.py
+++ font/hooks.py
@@ -11,7 +11,7 @@ app_license = "MIT"
 # ------------------
 
 # include js, css files in header of desk.html
-# app_include_css = "/assets/font/css/font.css"
+app_include_css = "/assets/font/css/font.css"
 # app_include_js = "/assets/font/js/font.js"
 
 # include js, css files in header of web template
